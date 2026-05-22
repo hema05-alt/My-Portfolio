ef6f471ebed090762b2684cfd94d7e7cd3e31879
Hemapriya <hemapriyalakshmi05@gmail .com>
2026-05-22 17:39:00 +0530

    First commit

---
+++ projects.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".project-typing", {
+
+    strings: [
+        "Travelsy Website"
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+
+
+});
---
+++ script.js
@@ -0,0 +1,35 @@
+var typed = new Typed(".typing", {
+
+    strings: [
+        "MCA Student",
+        "Frontend Learner",
+        "UI Designer"
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+});
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach((element) => {
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = element.getBoundingClientRect().top;
+
+        const revealPoint = 100;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            element.classList.add("active");
+
+        }
+
+    });
+
+});
